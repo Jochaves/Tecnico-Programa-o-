89ad78f8015acc6ef6c6be9fe809c79e979404b8
João Chaves <89935861+Jochaves@users.noreply.github.com>
2023-01-19 22:25:43 -0300

    Add files via upload

---
+++ SolucaoConsoleApp1/ProjetoConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Aluno!");
+
+// --- Nome e idade ---
+
+string nome, sobrenome; //declaração de variáveis 
+int idade;
+
+Console.Write("Digite seu nome: "); // Saída
+nome = Console.ReadLine(); // entrada 
+
+Console.Write("Digite seu sobrenome: "); // Saída
+sobrenome = Console.ReadLine(); // entrada
+
+Console.WriteLine("Digite sua idade: "); // Saída
+idade = int.Parse(Console.ReadLine()); // entrada  // int.Perse converte em um valor inteiro 
+
+Console.WriteLine();
+Console.WriteLine("---------------------");
+Console.WriteLine();
+
+// concatenando saída
+Console.Write(nome + " " + sobrenome + " tem " + idade + " anos de idade. ");
+Console.ReadKey(); //interrompe execução
