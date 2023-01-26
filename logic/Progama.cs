b1aaa5ac47b53047b66ba88fc292c87a066cc067
João Chaves <89935861+Jochaves@users.noreply.github.com>
2023-01-26 19:31:07 -0300

    Add files via upload

---
+++ logic/Progama.cs
@@ -0,0 +1,23 @@
+﻿string sexo;
+Console.WriteLine ("Digite 'h' para homem, 'm' para mulher ou qualquer tecla para sair: ");
+
+sexo = Console.ReadLine();
+if (sexo == "h" || sexo == "H")
+{
+    Console.WriteLine("sexo masculino");
+
+}
+else if ((sexo == "m" || sexo == "M"))
+{
+    Console.WriteLine("sexo feminino");
+}
+else
+{
+    Environment.Exit(0);
+}
+Console.ReadKey(); 
+
+
+
+
+
